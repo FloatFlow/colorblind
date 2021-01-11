c006bbe3253158a908e8c545bed265dc633e6103
FloatFlow <wolfrahfeldt@gmail.com>
2021-01-10 23:06:40 -0500

    added LAB color shift

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="colorblind-floatflow", # Replace with your own username
-    version="0.0.1",
+    name="colorblind",
+    version="0.0.2",
     author="floatflow",
-    #author_email="author@example.com",
+    author_email="wolfrahfeldt@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
